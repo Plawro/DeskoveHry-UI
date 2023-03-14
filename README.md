95938c8e6fdc6c4881d2ab9a4be081eb3d569905
Plawro <ales.hanacekit@seznam.cz>
2023-03-14 18:31:02 +0100

    Added files

---
+++ README.md
@@ -0,0 +1,4 @@
+# NotWorking_Ignore
+bro idk what im doimng help me pls
+<hr>
+hh what is wrooong 
---
+++ src/MainFrame.java
@@ -0,0 +1,136 @@
+
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class MainFrame extends JFrame{
+    private JFileChooser chooser = new JFileChooser(".");
+    ArrayList<JRadioButton> rateButtons = new ArrayList<>();
+    private JRadioButton rateButton1;
+    private JRadioButton rateButton2;
+    private JRadioButton rateButton3;
+
+    private JCheckBox ownedButton;
+    private JButton previousButton;
+    private JButton nextButton;
+
+    private JButton button1;
+    private JTextField textField1;
+    private JPanel mainPanel;
+    private JTextField textField2;
+    String line;
+
+    ArrayList<String> nazevHry = new ArrayList();
+    ArrayList<String> vlastnimeHru = new ArrayList();
+    ArrayList<Integer> oblibenostHry = new ArrayList();
+    static String separator = "-";
+    int aktualniDeskovka;
+
+    public MainFrame(){
+        initComponents();
+        rateButtons.add(0, rateButton1);
+        rateButtons.add(1, rateButton2);
+        rateButtons.add(2, rateButton3);
+    }
+    private void readFromFile() throws FileNotFoundException {
+        int result = chooser.showOpenDialog(this);
+        // Klikl uživatel na "Open"? Pokud ano, zpracuj událost
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = chooser.getSelectedFile();
+            textField2.setText(selectedFile.getAbsoluteFile().toString());
+
+            loadFile(selectedFile);
+            zobrazAktualniDeskovku();
+        }
+    }
+
+    private void loadFile(File selectedFile) throws FileNotFoundException {
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(selectedFile)))) {
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                String parametry[] = line.split(separator);
+                nazevHry.add(parametry[0]);
+                vlastnimeHru.add(parametry[1]);
+                oblibenostHry.add(Integer.parseInt(parametry[2]));
+            }
+        }catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+    private void initComponents() {
+        setTitle("Deskové hry");
+        setContentPane(mainPanel);
+        button1.addActionListener(e -> {
+            try {
+                readFromFile();
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        nextButton.addActionListener(e -> {
+            dalsiDeskovka(1);
+        });
+
+        previousButton.addActionListener(e -> {
+            dalsiDeskovka(-1);
+        });
+    }
+
+
+
+    private void dalsiDeskovka(int smer){
+        int pocetDeskovek = nazevHry.size();
+        if(pocetDeskovek == 0){
+            aktualniDeskovka = 0;
+            return;
+        }
+
+        aktualniDeskovka += smer;
+
+        if(aktualniDeskovka >= pocetDeskovek){
+            aktualniDeskovka = 0;
+        }
+
+        if(aktualniDeskovka <= -1){
+            aktualniDeskovka = pocetDeskovek-1;
+        }
+        zobrazAktualniDeskovku();
+    }
+
+
+    private void zobrazAktualniDeskovku() {
+        if(nazevHry.size() == 0){
+            textField1.setText("V seznamu nic není");
+        }else {
+            textField1.setText(nazevHry.get(aktualniDeskovka));
+
+            for (int i = 0; i < rateButtons.size(); i++) { //3 jsou radio buttony
+                rateButtons.get(i).setSelected(i == aktualniDeskovka);
+            }
+
+            if(vlastnimeHru.get(aktualniDeskovka).equals("Ano")){
+                ownedButton.setSelected(true);
+            }else{
+                ownedButton.setSelected(false);
+            }
+
+        }
+    }
+
+    public  static  void main(String[] args){
+        MainFrame frame = new MainFrame();
+        frame.setSize(600,200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+
+
+
